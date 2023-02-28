ff665157dfeac86d6c0c128887351b07f261e2d8
gustavoventieri <gustavo.mariano01@etec.sp.gov.br>
2023-02-28 11:14:05 -0300

    Aula 28/02 PW II

---
+++ resources/views/empresa.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site | Empresa</title>
+</head>
+<body>
+    <h1>Página da Empresa</h1>
+    <a href="/"><h2>Clique aqui para acessar a página inicial.</h2>
+    <a href="/produtos"><h2>Clique aqui para acessar os nossos produtos.</h2>
+    <a href="/contatos"><h2>Clique aqui para acessar os nossos contatos.</h2>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site | Home</title>
+</head>
+<body>
+    <h1>Página Inicial</h1>
+    <a href="/produtos"><h2>Clique aqui para acessar os nossos produtos.</h2>
+    <a href="/empresas"><h2>Clique aqui para acessar a nossa empresa.</h2>
+    <a href="/contatos"><h2>Clique aqui para acessar os nossos contatos.</h2>
+</html>
